a580482bda7ae5d9bded05a095d41b44980aba4e
FrancescaMancini <framan@ceh.ac.uk>
2023-09-22 10:08:37 +0000

    Add legend to plots

--- server.R
+++ server.R
@@ -46,36 +46,43 @@ ind_occ_plot <- reactive({
   ind_occ_data <- isolate(ind_occ_data())
   spp_occ_data <- isolate(spp_occ_data())
 
+ 
+  
   line_plot <- ind_occ_data %>%
-  plot_ly(x = ~year, y = ~upper, type = 'scatter', mode = 'lines',
-          line = list(color = 'transparent'),
-          showlegend = FALSE, name = 'Upper CI') %>%
-    add_trace(y = ~lower, type = 'scatter', mode = 'lines',
-              fill = 'tonexty', fillcolor='rgba(0,100,80,0.2)', 
-              line = list(color = 'transparent'),
+    plot_ly(x = ~year) %>%
+    add_ribbons(ymin = ~lower, ymax = ~upper, 
+                line = list(color = 'transparent'),
+                fillcolor = 'rgba(0,100,80,0.2)', 
+                name = '95% CI', hoverinfo = 'none',
+                showlegend = TRUE) %>%
+    add_lines(y = ~lower, line = list(color = 'transparent'),
               showlegend = FALSE, name = 'Lower CI') %>%
+    add_lines(y = ~upper, line = list(color = 'transparent'),
+              showlegend = FALSE, name = 'Upper CI') %>%
     add_trace(y = ~indicator, type = 'scatter', mode = 'lines',
-              line = list(color='rgba(0,100,80)'),
-              name = 'Indicator') %>%
+              line = list(color='rgb(0,100,80)'),
+              showlegend = TRUE, name = 'Indicator') %>%
     layout(xaxis = list(title = 'Year'), 
            yaxis = list(range = c(0, max(ind_occ_data$upper)+5),
-                        title = 'Occupancy index'))
+                        title = 'Occupancy index',
+                        hoverformat = ".2f"))
+  
   
   barplot <- spp_occ_data %>%
     plot_ly(x = ~time_period, y = ~proportion_species,
-                    type = "bar", showlegend=FALSE,
+                    type = "bar", showlegend=TRUE,
                     color = ~category) %>% 
     layout(barmode = "stack",
-                   xaxis = list(title = '', tickvals = c('st', 'lt'),
-                                ticktext = c('Short term', 'Long term')),
-                   yaxis = list(title = 'Proportion of species',
-                                hoverformat = '.2%',
-                                zeroline = FALSE,
-                                showline = FALSE,
-                                showticklabels = FALSE,
-                                showgrid = FALSE))  
+           xaxis = list(title = '', tickvals = c('st', 'lt'),
+                        ticktext = c('Short term', 'Long term')),
+           yaxis = list(title = 'Proportion of species',
+                        hoverformat = '.0%',
+                        tickvals = list(0, 0.25, 0.5, 0.75, 1),
+                        tickformat = '.0%'))  
   
- subplot(line_plot,barplot, widths = c(0.7, 0.3)) |> 
+ subplot(line_plot,barplot, widths = c(0.7, 0.3),
+         margin = 0.05, shareX = FALSE, shareY = FALSE,
+         titleX = TRUE, titleY = TRUE) |> 
     layout(title = "")
 
 })
@@ -135,39 +142,64 @@ ind_abnd_plot <- reactive({
   spp_abnd_data <- isolate(spp_abnd_data())
   
   
-  line_plot <- ind_abnd_data %>%
-    plot_ly(x = ~year, y = ~upper*100, type = 'scatter', mode = 'lines',
-            line = list(color = 'transparent'),
-            showlegend = FALSE, name = 'Upper CI') %>%
-    add_trace(y = ~lower*100, type = 'scatter', mode = 'lines',
-              fill = 'tonexty', fillcolor='rgba(0,100,80,0.2)',
-              line = list(color = 'transparent'),
-              showlegend = FALSE, name = 'Lower CI') %>%
-    add_trace(y = ~indicator*100, type = 'scatter', mode = 'lines',
-              line = list(color='rgba(0,100,80)'),
-              name = 'Indicator') %>%
-    add_trace(y = ~indicator_unsm*100, type = 'scatter', mode = 'markers',
-              marker = list(color='rgb(0,100,80)'),
-              name = 'Indicator unsmoothed') %>%
-    layout(xaxis = list(title = 'Year'),
-           yaxis = list(range = c(0, max(ind_abnd_data$indicator_unsm*100)+5),
-                        title = 'Abundance index'))
+  # line_plot <- ind_abnd_data %>%
+  #   plot_ly(x = ~year, y = ~upper*100, type = 'scatter', mode = 'lines',
+  #           line = list(color = 'transparent'),
+  #           showlegend = FALSE, name = 'Upper CI') %>%
+  #   add_trace(y = ~lower*100, type = 'scatter', mode = 'lines',
+  #             fill = 'tonexty', fillcolor='rgba(0,100,80,0.2)',
+  #             line = list(color = 'transparent'),
+  #             showlegend = FALSE, name = 'Lower CI') %>%
+  #   add_trace(y = ~indicator*100, type = 'scatter', mode = 'lines',
+  #             line = list(color='rgba(0,100,80)'),
+  #             name = 'Indicator') %>%
+  #   add_trace(y = ~indicator_unsm*100, type = 'scatter', mode = 'markers',
+  #             marker = list(color='rgb(0,100,80)'),
+  #             name = 'Indicator unsmoothed') %>%
+  #   layout(xaxis = list(title = 'Year'),
+  #          yaxis = list(range = c(0, max(ind_abnd_data$indicator_unsm*100)+5),
+  #                       title = 'Abundance index',
+  #                       hoverformat = '.2f'))
   
+  
+  line_plot <- ind_abnd_data %>%
+    plot_ly(x = ~year) %>%
+    add_ribbons(ymin = ~lower*100, ymax = ~upper*100, 
+                line = list(color = 'transparent'),
+                fillcolor = 'rgba(0,100,80,0.2)', 
+                name = '95% CI', hoverinfo = 'none',
+                showlegend = TRUE) %>%
+    add_lines(y = ~lower*100, line = list(color = 'transparent'),
+              showlegend = FALSE, name = 'Lower CI') %>%
+    add_lines(y = ~upper*100, line = list(color = 'transparent'),
+              showlegend = FALSE, name = 'Upper CI') %>%
+    add_trace(y = ~indicator*100, type = 'scatter', mode = 'lines',
+              line = list(color='rgb(0,100,80)'),
+              showlegend = TRUE, name = 'Indicator') %>%
+      add_trace(y = ~indicator_unsm*100, type = 'scatter', mode = 'markers',
+                marker = list(color='rgb(0,100,80)'),
+                name = 'Indicator unsmoothed', showlegend = TRUE) %>%
+      layout(xaxis = list(title = 'Year'),
+             yaxis = list(range = c(0, max(ind_abnd_data$indicator_unsm*100)+5),
+                          title = 'Abundance index',
+                          hoverformat = '.2f'))
+    
   barplot <- spp_abnd_data %>%
     plot_ly(x = ~period, y = ~cat_prop/100,
-            type = "bar", showlegend=FALSE,
+            type = "bar", showlegend=TRUE,
             color = ~cat_chg) %>% 
     layout(barmode = "stack",
            xaxis = list(title = '', tickvals = c('short-term', 'long-term'),
                         ticktext = c('Short term', 'Long term')),
            yaxis = list(title = 'Proportion of species',
-                        hoverformat = '.2%',
-                        zeroline = FALSE,
-                        showline = FALSE,
-                        showticklabels = FALSE,
-                        showgrid = FALSE))   
+                        position = "right",
+                        hoverformat = '.0%',
+                        tickvals = list(0, 0.25, 0.5, 0.75, 1),
+                        tickformat = '.0%'))  
   
-  subplot(line_plot,barplot, widths = c(0.7, 0.3)) |> 
+  subplot(line_plot,barplot, widths = c(0.7, 0.3),
+          margin = 0.05, shareX = FALSE, shareY = FALSE,
+          titleX = TRUE, titleY = TRUE) |> 
     layout(title = "")
   
 })
